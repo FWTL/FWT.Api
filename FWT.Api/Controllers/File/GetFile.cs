189320c1e810a42651f3b50f960c6ffb24ad81c7
Andrzej Golaszewski <agolasz@softserveinc.com>
2018-12-04 09:40:38 +0100

    get file wip

---
+++ FWT.Api/Controllers/File/GetFile.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using FWT.Core.CQRS;
+using FWT.Core.Helpers;
+using FWT.Core.Services.Telegram;
+using FWT.Core.Services.Unique;
+using FWT.Infrastructure.Telegram;
+using OpenTl.ClientApi;
+using OpenTl.Schema.Contacts;
+
+namespace FWT.Api.Controllers.File
+{
+    public class GetFile
+    {
+        public class Query : IQuery
+        {
+            public string PhoneHashId { get; set; }
+        }
+
+        public class Handler : IQueryHandler<Query, FileInfo>
+        {
+            private readonly ITelegramService _telegramService;
+
+            public IGuidService _guidService { get; }
+
+            public Handler(ITelegramService telegramService, IGuidService guidService)
+            {
+                _telegramService = telegramService;
+                _guidService = guidService;
+            }
+
+            public async Task<FileInfo> HandleAsync(Query query)
+            {
+                IClientApi client = await _telegramService.BuildAsync(query.PhoneHashId);
+                byte[] result = (await TelegramRequest.Handle(() =>
+                {
+                    return client.FileService.DownloadFullFileAsync(null);
+                }));
+
+                return new FileInfo()
+                {
+                    Content = result,
+                    Name = _guidService.New().ToString("n")
+                };
+            }
+    }
+}
---
+++ FWT.Api/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FWT.Api.Controllers.File;
+using FWT.Core.CQRS;
+using FWT.Core.Helpers;
+using FWT.Core.Services.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWT.Api.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+        private readonly ICurrentUserProvider _userProvider;
+
+        public FileController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher, ICurrentUserProvider userProvider)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+            _userProvider = userProvider;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<FileInfo> GetChats()
+        {
+            return await _queryDispatcher.DispatchAsync<GetFile.Query, FileInfo>(new GetFile.Query()
+            {
+                PhoneHashId = _userProvider.PhoneHashId(User)
+            });
+        }
+    }
+}
