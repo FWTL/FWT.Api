e551d9f6687dab0738e8a5b3803ca4432af1bf5e
Andrzej Gołaszewski <golaszewski.a@gmail.com>
2018-12-30 19:24:37 +0100

    use common package

--- FWTL.Api/Jobs/JobSetup.cs
+++ FWTL.Api/Jobs/JobSetup.cs
@@ -11,20 +11,23 @@
             database.Execute(conn =>
             {
                 conn.Execute(@"
-                ALTER TABLE [HangFire].[State] DROP CONSTRAINT [FK_HangFire_State_Job];
-                ALTER TABLE [HangFire].[JobParameter] DROP CONSTRAINT [FK_HangFire_JobParameter_Job];
-                DROP TABLE [HangFire].[Schema];
-                DROP TABLE [HangFire].[Job];
-                DROP TABLE [HangFire].[State];
-                DROP TABLE [HangFire].[JobParameter];
-                DROP TABLE [HangFire].[JobQueue];
-                DROP TABLE [HangFire].[Server];
-                DROP TABLE [HangFire].[List];
-                DROP TABLE [HangFire].[Set];
-                DROP TABLE [HangFire].[Counter];
-                DROP TABLE [HangFire].[Hash];
-                DROP TABLE [HangFire].[AggregatedCounter];
-                DROP SCHEMA [HangFire];");
+                IF OBJECT_ID('HangFire.State', 'U') IS NOT NULL
+                BEGIN
+                    ALTER TABLE [HangFire].[State] DROP CONSTRAINT [FK_HangFire_State_Job];
+                    ALTER TABLE [HangFire].[JobParameter] DROP CONSTRAINT [FK_HangFire_JobParameter_Job];
+                    DROP TABLE [HangFire].[Schema];
+                    DROP TABLE [HangFire].[Job];
+                    DROP TABLE [HangFire].[State];
+                    DROP TABLE [HangFire].[JobParameter];
+                    DROP TABLE [HangFire].[JobQueue];
+                    DROP TABLE [HangFire].[Server];
+                    DROP TABLE [HangFire].[List];
+                    DROP TABLE [HangFire].[Set];
+                    DROP TABLE [HangFire].[Counter];
+                    DROP TABLE [HangFire].[Hash];
+                    DROP TABLE [HangFire].[AggregatedCounter];
+                    DROP SCHEMA [HangFire];
+                END");
             });
         }
     }
